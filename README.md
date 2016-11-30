68da22d05af36e8afa0f1bd6b083631ce97235ee
Douglas Gordon <douglastgordon@gmail.com>
2016-11-30 15:27:09 -0500

    fix readme

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 BaseAnything is a Ruby Gem that allows you to create a number system of any base using any symbols you want.
 
+##Installation
+```ruby
+gem install 'baseanything'
+```
+
 ##Usage
+
+```ruby
+require 'baseanything'
+```
 
 ###Making your own number system
 
@@ -16,7 +25,7 @@ symbols = ['@', '#', '$', '%', '&']
 And create the number system like so:
 
 ```ruby
-NumberSystem.new(symbols)
+BaseAnything::NumberSystem.new(symbols)
 ```
 
 And each symbol will have the following decimal values:
@@ -41,7 +50,7 @@ So:
 You can take a number written in your base system and convert it to any base up to 35 with the to_base(num, base) method.
 
 ```ruby
-my_base_4 = NumberSystem.new(['@', '#', '$', '%', '&'])
+my_base_4 = BaseAnything::NumberSystem.new(['@', '#', '$', '%', '&'])
 my_num = '%#'
 target_base = 4
 
@@ -81,7 +90,7 @@ my_base_4.to_hexatrigesimal(num) #converts num to base36
 You can take a number written in any base system and convert it to your base system using the from_base(num, base) method.
 
 ```ruby
-my_base_4 = NumberSystem.new(['@', '#', '$', '%', '&'])
+my_base_4 = BaseAnything::NumberSystem.new(['@', '#', '$', '%', '&'])
 my_num = '100011'
 base_of_my_num = 2
 
@@ -92,26 +101,26 @@ my_base_4.from_base(my_num, target_base)
 Alternatively, you can use any of the following semantic methods:
 
 ```ruby
-my_base_4.to_binary(num) #converts num to base2
-my_base_4.to_ternary(num) #converts num to base3
-my_base_4.to_quaternary(num) #converts num to base4
-my_base_4.to_quinary(num) #converts num to base5
-my_base_4.to_senary(num) #converts num to base6
-my_base_4.to_heptary(num) #converts num to base7
-my_base_4.to_octal(num) #converts num to base8
-my_base_4.to_nonary(num) #converts num to base9
-my_base_4.to_decimal(num) #converts num to base10
-my_base_4.to_undecimal(num) #converts num to base11
-my_base_4.to_duodecimal(num) #converts num to base12
-my_base_4.to_tridecimal(num) #converts num to base13
+my_base_4.from_binary(num) #converts num from base2
+my_base_4.from_ternary(num) #converts num from base3
+my_base_4.from_quaternary(num) #converts num from base4
+my_base_4.from_quinary(num) #converts num from base5
+my_base_4.from_senary(num) #converts num from base6
+my_base_4.from_heptary(num) #converts num from base7
+my_base_4.from_octal(num) #converts num from base8
+my_base_4.from_nonary(num) #converts num from base9
+my_base_4.from_decimal(num) #converts num from base10
+my_base_4.from_undecimal(num) #converts num from base11
+my_base_4.from_duodecimal(num) #converts num from base12
+my_base_4.from_tridecimal(num) #converts num from base13
 
-my_base_4.to_tetradecimal(num) #converts num to base14
-my_base_4.to_pentadecimal(num) #converts num to base15
-my_base_4.to_hexadecimal(num) #converts num to base16
-my_base_4.to_vigesimal(num) #converts num to base20
-my_base_4.to_hexavigesimal(num) #converts num to base24
-my_base_4.to_heptavigesimal(num) #converts num to base27
-my_base_4.to_trigesimal(num) #converts num to base30
-my_base_4.to_duotrigesimal(num) #converts num to base32
-my_base_4.to_hexatrigesimal(num) #converts num to base36
+my_base_4.from_tetradecimal(num) #converts num from base14
+my_base_4.from_pentadecimal(num) #converts num from base15
+my_base_4.from_hexadecimal(num) #converts num from base16
+my_base_4.from_vigesimal(num) #converts num from base20
+my_base_4.from_hexavigesimal(num) #converts num from base24
+my_base_4.from_heptavigesimal(num) #converts num from base27
+my_base_4.from_trigesimal(num) #converts num from base30
+my_base_4.from_duotrigesimal(num) #converts num from base32
+my_base_4.from_hexatrigesimal(num) #converts num from base36
 ```
---
+++ spec/baseanything_spec.rb
@@ -0,0 +1,114 @@
+require 'rspec'
+require 'baseanything'
+
+describe '#to_base' do
+  base_two = ['0', '1']
+  base_four = ['0', '1', '2', '3']
+  base_seven = ['0', '1', '2', '3', '4', '5', '6']
+  base_eleven = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A']
+  let(:base_two_sys) { NumberSystem.new(base_two) }
+  let(:base_four_sys) { NumberSystem.new(base_four) }
+  let(:base_seven_sys) { NumberSystem.new(base_seven) }
+  let(:base_eleven_sys) { NumberSystem.new(base_eleven) }
+
+  it "converts base2 to base4" do
+    expect(base_two_sys.to_quaternary("0")).to eq("0")
+    expect(base_two_sys.to_quaternary("1")).to eq("1")
+    expect(base_two_sys.to_quaternary("100")).to eq("10")
+    expect(base_two_sys.to_quaternary("1011")).to eq("23")
+  end
+
+  it "converts base2 to base7" do
+    expect(base_two_sys.to_heptary("10011")).to eq("25")
+    expect(base_two_sys.to_heptary("1110")).to eq("20")
+    expect(base_two_sys.to_heptary("11101")).to eq("41")
+    expect(base_two_sys.to_heptary("01011")).to eq("14")
+  end
+
+  it "converts base2 to base11" do
+    expect(base_two_sys.to_undecimal("10011")).to eq("18")
+    expect(base_two_sys.to_undecimal("1110")).to eq("13")
+    expect(base_two_sys.to_undecimal("11101")).to eq("27")
+    expect(base_two_sys.to_undecimal("01011")).to eq("10")
+  end
+
+  it "converts base4 to base2" do
+    expect(base_four_sys.to_binary("10011")).to eq("100000101")
+    expect(base_four_sys.to_binary("32120")).to eq("1110011000")
+    expect(base_four_sys.to_binary("112")).to eq("10110")
+    expect(base_four_sys.to_binary("1312")).to eq("1110110")
+  end
+
+  it "converts base4 to base7" do
+    expect(base_four_sys.to_heptary("10011")).to eq("522")
+    expect(base_four_sys.to_heptary("32120")).to eq("2453")
+    expect(base_four_sys.to_heptary("112")).to eq("31")
+    expect(base_four_sys.to_heptary("1312")).to eq("226")
+  end
+
+  it "converts base4 to base11" do
+    expect(base_four_sys.to_undecimal("10011")).to eq("218")
+    expect(base_four_sys.to_undecimal("32120")).to eq("767")
+    expect(base_four_sys.to_undecimal("112")).to eq("20")
+    expect(base_four_sys.to_undecimal("1312")).to eq("A8")
+  end
+
+  it "converts base7 to base2" do
+    expect(base_seven_sys.to_binary("6433")).to eq("100011100110")
+    expect(base_seven_sys.to_binary("203201")).to eq("1000011110110110")
+    expect(base_seven_sys.to_binary("5510")).to eq("11110101111")
+    expect(base_seven_sys.to_binary("5")).to eq("101")
+  end
+
+  it "converts base7 to base4" do
+    expect(base_seven_sys.to_quaternary("6433")).to eq("203212")
+    expect(base_seven_sys.to_quaternary("203201")).to eq("20132312")
+    expect(base_seven_sys.to_quaternary("5510")).to eq("132233")
+    expect(base_seven_sys.to_quaternary("5")).to eq("11")
+  end
+
+  it "converts base7 to base11" do
+    expect(base_seven_sys.to_undecimal("6433")).to eq("1791")
+    expect(base_seven_sys.to_undecimal("203201")).to eq("24114")
+    expect(base_seven_sys.to_undecimal("5510")).to eq("1529")
+    expect(base_seven_sys.to_undecimal("5")).to eq("5")
+  end
+
+  it "converts base11 to base2" do
+    expect(base_eleven_sys.to_binary("9A812")).to eq("100011101010001100")
+    expect(base_eleven_sys.to_binary("937566")).to eq("101101111000010010000")
+    expect(base_eleven_sys.to_binary("A31")).to eq("10011011100")
+    expect(base_eleven_sys.to_binary("5")).to eq("101")
+  end
+
+  it "converts base11 to base4" do
+    expect(base_eleven_sys.to_quaternary("9A812")).to eq("203222030")
+    expect(base_eleven_sys.to_quaternary("937566")).to eq("11233002100")
+    expect(base_eleven_sys.to_quaternary("A31")).to eq("103130")
+    expect(base_eleven_sys.to_quaternary("5")).to eq("11")
+  end
+
+  it "converts base11 to base7" do
+    expect(base_eleven_sys.to_heptary("9A812")).to eq("1145555")
+    expect(base_eleven_sys.to_heptary("937566")).to eq("15531010")
+    expect(base_eleven_sys.to_heptary("A31")).to eq("3425")
+    expect(base_eleven_sys.to_heptary("5")).to eq("5")
+  end
+
+  describe '#from_base' do
+    crazy_base6 = ['%', '9', '(', '#', 'B', 'é']
+    let(:crazy_base6_sys) { NumberSystem.new(crazy_base6) }
+    it "converts from crazy base6" do
+      expect(crazy_base6_sys.from_binary("110001110001001101101")).to eq("éBéB(%Bé")
+      expect(crazy_base6_sys.from_quaternary("12032021231")).to eq("éBéB(%Bé")
+      expect(crazy_base6_sys.from_octal("6161155")).to eq("éBéB(%Bé")
+      expect(crazy_base6_sys.from_decimal("1630829")).to eq("éBéB(%Bé")
+      expect(crazy_base6_sys.from_tetradecimal("30647B")).to eq("éBéB(%Bé")
+      expect(crazy_base6_sys.from_pentadecimal("22331E")).to eq("éBéB(%Bé")
+      expect(crazy_base6_sys.from_hexavigesimal("4LN75")).to eq("éBéB(%Bé")
+    end
+
+
+  end
+
+end

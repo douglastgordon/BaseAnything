7591ea00ec1744217f68f8ff864a29b81435413e
Douglas Gordon <douglastgordon@gmail.com>
2016-11-29 22:22:21 -0500

    covert to any base

--- baseanything.rb
+++ baseanything.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class NumberSystem
 
   def initialize(arr)
@@ -18,12 +20,40 @@ class NumberSystem
   def to_dec(num)
     decimal_value = 0
     multiplier = 1
-    num.split("").each do |digit|
-      decimal_value += @correspondences[digit] * multipler
-      multipler *= @base
+    num.split("").reverse.each do |digit|
+      decimal_value += @correspondences[digit] * multiplier
+      multiplier *= @base
     end
     decimal_value
   end
 
+  def to_base(num, base)
+    num = to_dec(num)
+    output = ""
+    highest_quot = 0
+
+    while (base ** highest_quot) <= num
+      highest_quot += 1
+    end
+
+    while highest_quot >= 0
+      next_digit = num / (base ** highest_quot)
+      output << next_digit.to_s
+      to_remove = next_digit * (base ** highest_quot)
+      num -= to_remove
+      highest_quot -= 1
+    end
+
+    output
+
+  end
 
 end
+
+# arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+arr2 = ['0', '1']
+num_sys = NumberSystem.new(arr2)
+
+# num_sys.to_dec('91')
+
+p num_sys.to_base('1101', 10)

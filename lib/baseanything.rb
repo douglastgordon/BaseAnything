f4ae94a2380be0f7e401c46ea6642db59a5cf0ec
Douglas Gordon <douglastgordon@gmail.com>
2016-11-30 14:24:12 -0500

    convert to user system

--- lib/baseanything.rb
+++ lib/baseanything.rb
@@ -42,27 +42,27 @@ class NumberSystem
   }
 
   FIND_BASE = {
-    'to_binary' => 2,
-    'to_ternary' => 3,
-    'to_quaternary' => 4,
-    'to_quinary' => 5,
-    'to_senary' => 6,
-    'to_heptary' => 7,
-    'to_octal' => 8,
-    'to_nonary' => 9,
-    'to_decimal' => 10,
-    'to_undecimal' => 11,
-    'to_duodecimal' => 12,
-    'to_tridecimal' => 13,
-    'to_tetradecimal' => 14,
-    'to_pentadecimal' => 15,
-    'to_hexadecimal' => 16,
-    'to_vigesimal' => 20,
-    'to_hexavigesimal' => 24,
-    'to_heptavigesimal' => 27,
-    'to_trigesimal' => 30,
-    'to_duotrigesimal' => 32,
-    'to_hexatrigesimal' => 36
+    'binary' => 2,
+    'ternary' => 3,
+    'quaternary' => 4,
+    'quinary' => 5,
+    'senary' => 6,
+    'heptary' => 7,
+    'octal' => 8,
+    'nonary' => 9,
+    'decimal' => 10,
+    'undecimal' => 11,
+    'duodecimal' => 12,
+    'tridecimal' => 13,
+    'tetradecimal' => 14,
+    'pentadecimal' => 15,
+    'hexadecimal' => 16,
+    'vigesimal' => 20,
+    'hexavigesimal' => 24,
+    'heptavigesimal' => 27,
+    'trigesimal' => 30,
+    'duotrigesimal' => 32,
+    'hexatrigesimal' => 36
   }
 
   def initialize(arr)
@@ -80,18 +80,26 @@ class NumberSystem
     hash
   end
 
-  def to_dec(num)
+  def own_to_dec(num)
     decimal_value = 0
-    multiplier = 1
-    num.split("").reverse.each do |digit|
-      decimal_value += @correspondences[digit] * multiplier
-      multiplier *= @base
+    num.split("").reverse.each_with_index do |digit, i|
+      decimal_value += @correspondences[digit] * (@base**i)
+    end
+    decimal_value
+  end
+
+  def other_to_dec(num, base)
+    decimal_value = 0
+    num.split("").reverse.each_with_index do |digit, i|
+      digit_value = ANY_BASE.select{|k,v| ANY_BASE[k] == digit}
+      digit_value = digit_value.keys[0].to_i
+      decimal_value += digit_value * (base**i)
     end
     decimal_value
   end
 
   def to_base(num, base)
-    num = to_dec(num)
+    num = own_to_dec(num)
     output = ""
     highest_quot = 0
 
@@ -110,21 +118,55 @@ class NumberSystem
     output
   end
 
-  def method_missing(name, num)
-    name = name.to_s
-    base = FIND_BASE["to_#{name[3..-1]}"] if name[0..2] == "to_"
-    to_base(num, base)
+  def from_base(num, base)
+    num = other_to_dec(num, base)
+
+    output = ""
+    highest_quot = 0
+
+    while (@base ** (highest_quot+1) <= num)
+      highest_quot += 1
+    end
+
+    while highest_quot >= 0
+      next_digit = num / (@base ** highest_quot)
+      digit_value = @correspondences.select{|k,v| @correspondences[k.to_s] == next_digit}
+      output << digit_value.keys[0]
+      to_remove = next_digit * (@base ** highest_quot)
+      num -= to_remove
+      highest_quot -= 1
+    end
+
+    output
+
   end
 
-  
+  def method_missing(name, num)
+    name = name.to_s
+
+    if name[0..2] == "to_"
+      base = FIND_BASE["#{name[3..-1]}"]
+      to_base(num, base)
+    elsif name[0..4] == "from_"
+
+      base = FIND_BASE["#{name[5..-1]}"]
+      from_base(num, base)
+    end
+  end
+
+
+
 
 end
 
 # arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# arr2 = ['0', '1', '2', '3']
-# num_sys = NumberSystem.new(arr2)
+arr2 = ['0', '1', '2', '3']
+num_sys = NumberSystem.new(arr2)
 #
 # # num_sys.to_dec('91')
 #
 # p num_sys.to_base('11020', 10)
 # p num_sys.to_bin('123')
+# p num_sys.other_to_dec('21', 4)
+
+p num_sys.from_decimal('22')

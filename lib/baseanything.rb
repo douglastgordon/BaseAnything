225bfaf18b7ade376f24aa69e6fbb48bdd60c153
Douglas Gordon <douglastgordon@gmail.com>
2016-11-30 14:26:03 -0500

    clean-up

--- lib/baseanything.rb
+++ lib/baseanything.rb
@@ -2,100 +2,9 @@ require 'byebug'
 
 class NumberSystem
 
-  ANY_BASE = {
-    '0' => '0',
-    '1' => '1',
-    '2' => '2',
-    '3' => '3',
-    '4' => '4',
-    '5' => '5',
-    '6' => '6',
-    '7' => '7',
-    '8' => '8',
-    '9' => '9',
-    '10' => 'A',
-    '11' => 'B',
-    '12' => 'C',
-    '13' => 'D',
-    '14' => 'E',
-    '15' => 'F',
-    '16' => 'G',
-    '17' => 'H',
-    '18' => 'I',
-    '19' => 'J',
-    '20' => 'K',
-    '21' => 'L',
-    '22' => 'M',
-    '23' => 'N',
-    '24' => 'O',
-    '25' => 'P',
-    '26' => 'Q',
-    '27' => 'R',
-    '28' => 'S',
-    '29' => 'T',
-    '30' => 'U',
-    '31' => 'V',
-    '32' => 'W',
-    '33' => 'X',
-    '34' => 'Y',
-    '35' => 'Z'
-  }
-
-  FIND_BASE = {
-    'binary' => 2,
-    'ternary' => 3,
-    'quaternary' => 4,
-    'quinary' => 5,
-    'senary' => 6,
-    'heptary' => 7,
-    'octal' => 8,
-    'nonary' => 9,
-    'decimal' => 10,
-    'undecimal' => 11,
-    'duodecimal' => 12,
-    'tridecimal' => 13,
-    'tetradecimal' => 14,
-    'pentadecimal' => 15,
-    'hexadecimal' => 16,
-    'vigesimal' => 20,
-    'hexavigesimal' => 24,
-    'heptavigesimal' => 27,
-    'trigesimal' => 30,
-    'duotrigesimal' => 32,
-    'hexatrigesimal' => 36
-  }
-
   def initialize(arr)
     @correspondences = make_correspondance_hash(arr)
     @base = arr.length
-  end
-
-  def make_correspondance_hash(arr)
-    hash = {}
-    value = 0
-    arr.each do |key|
-      hash[key] = value
-      value += 1
-    end
-    hash
-  end
-
-  def own_to_dec(num)
-    decimal_value = 0
-    num.split("").reverse.each_with_index do |digit, i|
-      decimal_value += @correspondences[digit] * (@base**i)
-    end
-    decimal_value
-  end
-
-  def other_to_dec(num, base)
-    decimal_value = 0
-    num.split("").reverse.each_with_index do |digit, i|
-      digit_value = ANY_BASE.select{|k,v| ANY_BASE[k] == digit}
-      digit_value = digit_value.keys[0].to_i
-      decimal_value += digit_value * (base**i)
-    end
-    decimal_value
   end
 
   def to_base(num, base)
@@ -143,30 +52,108 @@ class NumberSystem
 
   def method_missing(name, num)
     name = name.to_s
-
     if name[0..2] == "to_"
       base = FIND_BASE["#{name[3..-1]}"]
       to_base(num, base)
     elsif name[0..4] == "from_"
-
       base = FIND_BASE["#{name[5..-1]}"]
       from_base(num, base)
     end
   end
 
+  private
 
+
+    ANY_BASE = {
+      '0' => '0',
+      '1' => '1',
+      '2' => '2',
+      '3' => '3',
+      '4' => '4',
+      '5' => '5',
+      '6' => '6',
+      '7' => '7',
+      '8' => '8',
+      '9' => '9',
+      '10' => 'A',
+      '11' => 'B',
+      '12' => 'C',
+      '13' => 'D',
+      '14' => 'E',
+      '15' => 'F',
+      '16' => 'G',
+      '17' => 'H',
+      '18' => 'I',
+      '19' => 'J',
+      '20' => 'K',
+      '21' => 'L',
+      '22' => 'M',
+      '23' => 'N',
+      '24' => 'O',
+      '25' => 'P',
+      '26' => 'Q',
+      '27' => 'R',
+      '28' => 'S',
+      '29' => 'T',
+      '30' => 'U',
+      '31' => 'V',
+      '32' => 'W',
+      '33' => 'X',
+      '34' => 'Y',
+      '35' => 'Z'
+    }
+
+    FIND_BASE = {
+      'binary' => 2,
+      'ternary' => 3,
+      'quaternary' => 4,
+      'quinary' => 5,
+      'senary' => 6,
+      'heptary' => 7,
+      'octal' => 8,
+      'nonary' => 9,
+      'decimal' => 10,
+      'undecimal' => 11,
+      'duodecimal' => 12,
+      'tridecimal' => 13,
+      'tetradecimal' => 14,
+      'pentadecimal' => 15,
+      'hexadecimal' => 16,
+      'vigesimal' => 20,
+      'hexavigesimal' => 24,
+      'heptavigesimal' => 27,
+      'trigesimal' => 30,
+      'duotrigesimal' => 32,
+      'hexatrigesimal' => 36
+    }
+
+  def make_correspondance_hash(arr)
+    hash = {}
+    value = 0
+    arr.each do |key|
+      hash[key] = value
+      value += 1
+    end
+    hash
+  end
+
+  def own_to_dec(num)
+    decimal_value = 0
+    num.split("").reverse.each_with_index do |digit, i|
+      decimal_value += @correspondences[digit] * (@base**i)
+    end
+    decimal_value
+  end
+
+  def other_to_dec(num, base)
+    decimal_value = 0
+    num.split("").reverse.each_with_index do |digit, i|
+      digit_value = ANY_BASE.select{|k,v| ANY_BASE[k] == digit}
+      digit_value = digit_value.keys[0].to_i
+      decimal_value += digit_value * (base**i)
+    end
+    decimal_value
+  end
 
 
 end
-
-# arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-arr2 = ['0', '1', '2', '3']
-num_sys = NumberSystem.new(arr2)
-#
-# # num_sys.to_dec('91')
-#
-# p num_sys.to_base('11020', 10)
-# p num_sys.to_bin('123')
-# p num_sys.other_to_dec('21', 4)
-
-p num_sys.from_decimal('22')
